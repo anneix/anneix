de7cdb50dd8fba8ee8636c05c7374aa91bb69935
anneix <annexkc4u@gmail.com>
2021-09-08 21:40:30 +0000

    my second commit

---
+++ sc2.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "Hello guys"
+echo "Welcome to shellscript"
+echo "This is the first shell script example!!"
+echo "Today's date is `date`"
+echo "Shellscript is very easy"
+echo "Learning DevOps is the best program"
+
